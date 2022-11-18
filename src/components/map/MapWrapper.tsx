9f76b417be927593c008e8dfc471d0d9160b46f3
Ricardo Moura Vargas <ricardo.mouravargas@intive.com>
2022-11-18 11:38:46 -0300

    created popup component, implemented reading coordinates from click

--- src/components/map/MapWrapper.tsx
+++ src/components/map/MapWrapper.tsx
@@ -1,22 +1,55 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Overlay } from 'ol';
 
 import { useMap } from '../../context/MapContext';
+
+import PopupCoordinate from '../popupCoordinate/PopupCoordinate';
 
 import 'ol/ol.css';
 import './mapWrapper.css';
 
 const MapWrapper = () => {
+  const [currentCoordinates, setCurrentCoordinates] = useState({ lat: '', long: '' });
+  const [existPopupOverlay, setExistPopupOverlay] = useState(false);
   const { dispatch } = useMap();
   const mapRef = useRef(null);
+  const popupRef = useRef(null);
 
   useEffect(() => {
     dispatch({
       type: 'create-map',
       payload: { mapRef: mapRef?.current ?? undefined },
     });
+    dispatch({ type: 'add-event', payload: { eventName: 'click', callback: onClickHandler } });
   }, []);
 
-  return <div ref={mapRef} id='ol-map' className='map-wrapper'></div>;
+  const onClickHandler = (e: any) => {
+    const popup = new Overlay({
+      element: popupRef?.current ?? undefined,
+    });
+
+    if (!existPopupOverlay) {
+      dispatch({ type: 'add-overlay', payload: { overlay: popup } });
+      setExistPopupOverlay(true);
+    }
+
+    const tempCoordinate = e.coordinate.join().split(',');
+    setCurrentCoordinates({ lat: tempCoordinate[0], long: tempCoordinate[1] });
+
+    popup?.setPosition(undefined);
+    popup?.setPosition(e.coordinate);
+  };
+
+  return (
+    <>
+      <PopupCoordinate
+        inputRef={popupRef}
+        lat={currentCoordinates.lat}
+        long={currentCoordinates.long}
+      />
+      <div ref={mapRef} id='ol-map' className='map-wrapper'></div>
+    </>
+  );
 };
 
 export default MapWrapper;
