9060254227dde404e7c710c9d5fecd07c9718305
Ricardo Moura Vargas <ricardo.mouravargas@intive.com>
2022-11-19 16:27:28 -0300

    RM-21/feat: Added copy and close buttons to the popupCoordinates

--- src/components/popupCoordinate/PopupCoordinate.tsx
+++ src/components/popupCoordinate/PopupCoordinate.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
 
+import Wrapper from '../wrapper/Wrapper';
+
+import './popupCoordinate.css';
+
 type PopupCoordinateProps = {
+  inputRef: React.MutableRefObject<null>;
   lat: number;
   long: number;
+  copyAction: Function;
+  closeAction: Function;
 };
 
-const PopupCoordinate = ({ lat, long }: PopupCoordinateProps) => (
-  <>
-    <p>
-      <strong>Lat:</strong>
-      {` ${lat}`}
-    </p>
-    <p>
-      <strong>Long:</strong>
-      {` ${long}`}
-    </p>
-  </>
+const PopupCoordinate = ({
+  inputRef,
+  lat,
+  long,
+  copyAction,
+  closeAction,
+}: PopupCoordinateProps) => (
+  <Wrapper inputRef={inputRef} styleClasses={['popup-coordinate']}>
+    <div>
+      <p>
+        <strong>Lat:</strong>
+        {` ${lat}`}
+      </p>
+      <p>
+        <strong>Long:</strong>
+        {` ${long}`}
+      </p>
+    </div>
+    <div className='popup__button-container'>
+      <button onClick={() => copyAction()}>
+        <img src='/assets/images/copy.svg' title='Copy coordinates' alt='copy coordinates' />
+      </button>
+      <button onClick={() => closeAction()}>
+        <img src='/assets/images/close.svg' title='Close' alt='close' />
+      </button>
+    </div>
+  </Wrapper>
 );
 
 export default PopupCoordinate;
